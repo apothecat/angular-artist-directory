a349c06292a4428c7abb25e50e7f7f8ac2e2287a
apothecat <catmcg1982@gmail.com>
2021-02-10 15:58:16 +0000

    Renames project.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/data-visualisation'),
+      dir: require('path').join(__dirname, './coverage/angular-artist-directory'),
       subdir: '.',
       reporters: [
         { type: 'html' },
