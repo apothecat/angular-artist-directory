b5dc5a0ff8bcc6faf5a4f587a2ec55622ed3055b
apothecat <catmcg1982@gmail.com>
2021-02-18 12:59:27 +0000

    Adds filter pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
